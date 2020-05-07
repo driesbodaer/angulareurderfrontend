c40132935e0e171462bb4ef01bebe796166e08b4
Dries Bodaer <dries.bodaer@cegeka.com>
2020-05-07 15:03:46 +0200

    first

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const application = express();
 
 application.use(express.static('./dist/Eurder-Angular-frontend'));
 
-application.listen(process.env.PORT || 8080);
+application.listen(process.env.PORT || 4200);
